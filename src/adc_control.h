4322b467f60bab1f7fd211c8b00cd43dcda8aefd
harmandubb <harmand1999@gmail.com>
2024-06-14 16:25:08 -0700

    added function prototypes to the adc control module

--- src/adc_control.h
+++ src/adc_control.h
@@ -21,28 +21,11 @@ extern "C" {
 #include <zephyr/kernel.h>
 #include <zephyr/device.h> //need to allow memory size in the heap to use malloc
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/pwm.h>
 #include <zephyr/logging/log.h>
-
-#include <dk_buttons_and_leds.h>
-
-#include <nrfx_pwm.h>
-#include <nrfx.h>
+#include <zephyr/types.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdlib.h>
-
-#define PWM_PERIOD PWM_NSEC(1250) //ns
-#define PWM_TOP_VALUE (20)
-#define PWM_NUM_LED (10)
-#define PWM_RESET_PERIODS (70)
-#define PWM_PULSE_ARRAY_SIZE (PWM_NUM_LED*4*8)
-#define PWM_ARRAY_SIZE PWM_PULSE_ARRAY_SIZE+PWM_RESET_PERIODS //need the last value for the reset
-#define PWM_0 (PWM_TOP_VALUE - 5) //clock ticks at 16, MHz
-#define PWM_1 (PWM_TOP_VALUE - 10) //clock ticks at 16 MHz
-#define RESET_CODE_DURATION (10) //number of periods to hold low 
-#define PIN_TOGGLE_OUTPUT (28)
-#define PIN_BUTTON_INTERFACE (3)
-
-
 
 #ifdef __cplusplus
 }
