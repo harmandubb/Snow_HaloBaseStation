7380457dc8f4be1719d5d014f14ef0380dc3c158
Harman <Harman@Docstruction.com>
2024-10-12 20:11:13 -0700

    Solve the overlay binding issue

--- Testing/UART/src/main.c
+++ Testing/UART/src/main.c
@@ -1,12 +1,3 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/* Controlling LEDs through UART. Press 1-3 on your keyboard to toggle LEDS 1-3 on your development
- * kit */
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gnss.h>
@@ -20,20 +11,65 @@ LOG_MODULE_REGISTER(gps_app, LOG_LEVEL_INF);
 
 #define GPS_NODE DT_NODELABEL(gps0)
 
+#if DT_NODE_EXISTS(GPS_NODE)
+#define GPS_DEVICE_READY 1
+#else
+#define GPS_DEVICE_READY 0
+#endif
+
 int main(void)
 {
-	int err = 0;
+    printk("Checking GPS node...\n");
+    
+    #if GPS_DEVICE_READY
+    printk("GPS node exists\n");
+    const struct device *gnss_dev = DEVICE_DT_GET(GPS_NODE);
 
-	static const struct device *gnss_dev = DEVICE_DT_GET(GPS_NODE);
-	// Check if the GNSS device is ready
     if (!device_is_ready(gnss_dev)) {
-        LOG_ERR("GNSS device not ready");
+        printk("GNSS device not ready\n");
         return -ENODEV;
     }
-	
-	
-	while (1) {
-		k_msleep(1000);
-		printk("System working in the loop\n");
-	}
+
+    printk("GNSS device is ready\n");
+    #else
+    printk("GPS node does not exist in the device tree\n");
+    #endif
+
+    while (1) {
+        k_msleep(1000);
+        printk("System working in the loop\n");
+    }
+
+    return 0;
 }
+
+// #include <zephyr/kernel.h>
+// #include <zephyr/device.h>
+// #include <zephyr/drivers/gnss.h>
+// #include <zephyr/devicetree.h>
+// #include <zephyr/drivers/gpio.h>
+// #include <zephyr/sys/printk.h>
+// #include <zephyr/logging/log.h>
+// #include <zephyr/drivers/uart.h>
+
+// LOG_MODULE_REGISTER(gps_app, LOG_LEVEL_INF);
+
+// #define GPS_NODE DT_NODELABEL(gps0)
+
+// int main(void)
+// {
+// 	int err = 0;
+
+// 	static const struct device *gnss_dev = DEVICE_DT_GET(GPS_NODE);
+// 	// Check if the GNSS device is ready
+//     if (!device_is_ready(gnss_dev)) {
+//         LOG_ERR("GNSS device not ready");
+//         return -ENODEV;
+//     }
+	
+	
+// 	while (1) {
+// 		k_msleep(1000);
+// 		printk("System working in the loop\n");
+// 	}
+// }
